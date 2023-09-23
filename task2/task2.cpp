e7531f547c9cb7e6ea0ab4e105ea252e4828465f
SediakinIvan <145792548+SediakinIvan@users.noreply.github.com>
2023-09-23 21:23:11 +0300

    task2.cpp

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -1,32 +1,75 @@
-﻿#include <iostream>
-#include <string>
+//А) Перед main() в комментариях ответьте на вопросы и напишите соответствия С – С++ для
+//следующих операций :
+//1) int* ptr = (int*)malloc(sizeof(int)) - int* ptr = new int
+//2) int* ptrArr = (int*)malloc(sizeof(int) * N) - int* ptrArr = new int[N]
+//3) free(ptr) - delete ptr
+//4) free(ptrArr) - delete[] ptrArr
+//В С++ необходимо ли освобождать динамическую память ? 
+// Да, оператором delete.
+//Можно ли освободить одну и ту же выделенную память дважды ? 
+// Нет, это приводит к недокументированному поведению.
+//А можно ли освободить невыделенную память ? 
+// Нет.
+// 
+// 
+// 
+//Б) Треугольник Фибоначчи.У некоторых это задание было на программировании по С.Теперь
+//требуется адаптировать эту программу(или написать заново) на С++ с определенными
+//требованиями(NB) :
+//  -именно пользователь должен решать(посредством ввода в командную строку) сколько строк у
+//  пирамиды должно быть;
+//-перемещаться по массиву(-ам) необходимо только через указательную арифметику – Pointer
+//Arithmetic[arr[i][j] - > *(*(arr + i) + j)];
+//-использовать только динамическое выделение памяти
+
+#include <iostream>
+
+int** createFibonacciTriangle(int rows) {
+    int** triangle = new int* [rows];
+
+    for (int i = 0; i < rows; i++) {
+        triangle[i] = new int[i + 1];
+        triangle[i][0] = 1;
+        triangle[i][i] = 1;
+
+        for (int j = 1; j < i; j++) {
+            *(triangle[i] + j) = *(triangle[i - 1] + j - 1) + *(triangle[i - 1] + j);
+        }
+    }
+
+    return triangle;
+}
+
+void printFibonacciTriangle(int** triangle, int rows) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j <= i; j++) {
+            std::cout << *(*(triangle + i) + j) << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void deleteFibonacciTriangle(int** triangle, int rows) {
+    for (int i = 0; i < rows; i++) {
+        delete[] triangle[i];
+    }
+    delete[] triangle;
+}
 
 int main() {
-    std::string input;
+    int rows;
 
-    std::cout << "Введите строку: ";
-    std::getline(std::cin, input);
+    std::cout << "Введите количество строк: ";
+    std::cin >> rows;
 
-    // Удаляем пробелы и пунктуационные знаки
-    std::string cleanedInput;
-    for (char c : input) {
-        if (!std::isspace(c) && !std::ispunct(c)) {
-            cleanedInput += c;
-        }
-    }
+    // Создание треугольника Фибоначчи
+    int** fibonacciTriangle = createFibonacciTriangle(rows);
 
-    // Сортируем буквы в алфавитном порядке
-    for (int i = 0; i < cleanedInput.length() - 1; i++) {
-        for (int j = 0; j < cleanedInput.length() - i - 1; j++) {
-            if (cleanedInput[j] > cleanedInput[j + 1]) {
-                char temp = cleanedInput[j];
-                cleanedInput[j] = cleanedInput[j + 1];
-                cleanedInput[j + 1] = temp;
-            }
-        }
-    }
+    // Вывод треугольника Фибоначчи
+    printFibonacciTriangle(fibonacciTriangle, rows);
 
-    std::cout << "Результат: " << cleanedInput << std::endl;
+    // Освобождение памяти
+    deleteFibonacciTriangle(fibonacciTriangle, rows);
 
     return 0;
 }
